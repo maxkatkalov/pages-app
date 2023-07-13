db5e00401adacb9dd0cff3f6e20105b926893336
Max Katkalov <makskatkalov@gmail.com>
2023-07-13 10:57:19 +0300

    urls changed x2

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ from .views import HomePageView, ContactPageView, \
 
 urlpatterns = [
     path("index.html", HomePageView.as_view(), name="home"),
-    # path("", HomePageView.as_view(), name="home1"),
+    path("", HomePageView.as_view(), name="home1"),
     path("contact.html", ContactPageView.as_view(), name="contact.html"),
     path("about-us.html", AboutUsPageView.as_view(), name="about"),
     path("services.html", ServicePageView.as_view(), name="services"),
